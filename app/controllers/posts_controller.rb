b915798cd43458e0e570c9c084036747061b9347
Daniel Falcon <Danfall963@Gmail.com>
2023-07-21 13:37:56 -0400

    added form to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  helper_method :current_user
+
   def index
     @user = current_user
     @posts = Post.all
@@ -7,5 +10,28 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def current_user
+    @current_user ||= User.first
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = User.find(params[:user_id]).posts.build(post_params)
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post creado exitosamente.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts/', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+    resources :users do
+      resources :posts do
+        resources :comments, only: [:create]
+        resources :likes, only: [:create]
+    end
+  end
 end
