e056c4223614fdba37f5c0ee90e9f17bf6e2736a
Daniel Falcon <Danfall963@Gmail.com>
2023-07-26 17:17:13 -0400

    added structure to features tests

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -9,20 +9,25 @@ RSpec.describe 'User Show Page', type: :feature do
     )
     visit user_path(@user)
   end
+
   it 'displays the user profile picture' do
     expect(page).to have_css('img[alt="userPhoto"]')
   end
+
   it 'displays the user username' do
     expect(page).to have_content(@user.name)
   end
+
   it 'displays the number of posts the user has written' do
     @user.update(posts_counter: 10)
     visit user_path(@user)
     expect(page).to have_content('Number of posts: 10')
   end
+
   it 'displays the user bio' do
     expect(page).to have_content(@user.bio)
   end
+
   it 'displays the user\'s first 3 posts' do
     recent_posts = @user.recent_posts.limit(3)
     recent_posts.each do |post|
@@ -30,15 +35,18 @@ RSpec.describe 'User Show Page', type: :feature do
       expect(page).to have_content(post.text)
     end
   end
+
   it 'displays a button to view all user posts' do
     expect(page).to have_button('View All Posts')
   end
+
   it 'redirects to the post\'s show page when clicking a user\'s post' do
     post = Post.create(title: 'Sample Post', text: 'Sample post content', author_id: @user.id)
     visit user_path(@user)
     click_link post.title
     expect(current_path).to eq(user_post_path(user_id: @user.id, id: post.id))
   end
+  
   it 'redirects to the user\'s post index page when clicking "View All Posts"' do
     posts = [
       Post.create(title: 'Post 1', text: 'Content 1', author_id: @user.id),
--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -8,21 +8,25 @@ RSpec.describe 'User index page', type: :feature do
     ]
     visit users_path
   end
+
   it 'displays the username of all other users' do
     @users.each do |user|
       expect(page).to have_content(user.name)
     end
   end
+
   it 'displays the profile picture for each user' do
     @users.each do |user|
       expect(page).to have_css("img[src*='#{user.photo}'][alt='userPhoto']")
     end
   end
+
   it 'displays the number of posts each user has written' do
     @users.each do |user|
       expect(page).to have_content("Number of posts: #{user.posts_counter}")
     end
   end
+  
   it 'redirects to the user show page when clicking on a user' do
     @users.each do |user|
       click_link user.name
