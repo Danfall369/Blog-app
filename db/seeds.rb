4c22ce49a06a9477a9ab393b3ef734ae9fa8e55a
Daniel Falcon <Danfall963@Gmail.com>
2023-07-28 05:39:42 -0400

    update seeds and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # db/seeds.rb
 
 # Create users
-first_user = User.create(name: 'Tom', photo: 'https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg?w=740&t=st=1689738009~exp=1689738609~hmac=5757be4c36da7b17920e83acece1487622aeb3b074b1cd30ca510073909f40db', email: 'tom@email.com', password: '123456', bio: 'Teacher from Mexico.', posts_counter: 0)
-second_user = User.create(name: 'Lilly', photo: 'https://img.freepik.com/foto-gratis/retrato-hermoso-mujer-joven-posicion-pared-gris_231208-10760.jpg?w=740&t=st=1689738007~exp=1689738607~hmac=8b20e5cdb0ee3ec7d948332a9279376fcb01f6eb493d6272b807390e7df83fbb', email: 'lily@email.com', password: '123456', bio: 'Teacher from Poland.', posts_counter: 0)
+first_user = User.create(name: 'Tom', photo: 'https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg?w=740&t=st=1689738009~exp=1689738609~hmac=5757be4c36da7b17920e83acece1487622aeb3b074b1cd30ca510073909f40db', email: 'tom@email.com', password: '123456', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://img.freepik.com/foto-gratis/retrato-hermoso-mujer-joven-posicion-pared-gris_231208-10760.jpg?w=740&t=st=1689738007~exp=1689738607~hmac=8b20e5cdb0ee3ec7d948332a9279376fcb01f6eb493d6272b807390e7df83fbb', email: 'lily@email.com', password: '123456', bio: 'Teacher from Poland.')
 
 # Create posts for the first user
 first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
