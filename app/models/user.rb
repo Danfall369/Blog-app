bc6a299ca68e161e7b899bbb1df5b83a784855c3
Daniel Falcon <Danfall963@Gmail.com>
2023-07-29 21:22:55 -0400

    finish all work on api end point

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,14 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def admin?
-    role == 'admin'
-  end
+  before_save :generate_api_token
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def generate_api_token
+    self.api_token = SecureRandom.hex(16)
+    puts "Generate API Token: #{api_token}"
   end
 end
